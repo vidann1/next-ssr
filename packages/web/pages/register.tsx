20f1d801e76a8af002e853a3066b76adafa96371
vidann1 <vidans233@gmail.com>
2018-12-25 20:21:53 +0100

    register

--- packages/web/pages/register.tsx
+++ packages/web/pages/register.tsx
@@ -1,10 +1,15 @@
 import { Button, Form } from "semantic-ui-react";
 import { Formik, Field } from "formik";
-
 import { registerSchema } from "@ssr/common";
-
 import { InputField } from "../components/formik-fields/InputField";
 import { ErrorMessage } from "../components/ErrorMessage";
+import { normalizeErrors } from "../utils/normalizeErrors";
+import { Mutation } from "react-apollo";
+import { registerMutation } from "../graphql/user/mutation/register";
+import {
+  RegisterMutation,
+  RegisterMutationVariables
+} from "../lib/schema-types";
 
 interface FormValues {
   username: string;
@@ -15,56 +20,71 @@ interface FormValues {
 }
 
 export default () => (
-  <Formik<FormValues>
-    initialValues={{
-      username: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: ""
-    }}
-    onSubmit={() => {}}
-    validationSchema={registerSchema}
-    validateOnBlur={false}
-    validateOnChange={false}
+  <Mutation<RegisterMutation, RegisterMutationVariables>
+    mutation={registerMutation}
   >
-    {({ errors, handleSubmit }) => (
-      <Form onSubmit={handleSubmit}>
-        <Field
-          name="username"
-          label="Username"
-          placeholder="Username"
-          component={InputField}
-        />
+    {mutate => (
+      <Formik<FormValues>
+        initialValues={{
+          username: "",
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: ""
+        }}
+        onSubmit={async (input, { setErrors, setSubmitting }) => {
+          const response = await mutate({
+            variables: { input }
+          });
 
-        <Field
-          name="firstName"
-          label="First Name"
-          placeholder="First Name"
-          component={InputField}
-        />
-        <Field
-          name="lastName"
-          label="Last Name"
-          placeholder="Last Name"
-          component={InputField}
-        />
-        <Field
-          name="email"
-          label="Email"
-          placeholder="Email"
-          component={InputField}
-        />
-        <Field
-          name="password"
-          label="Password"
-          placeholder="Password"
-          component={InputField}
-          type="password"
-        />
-        <ErrorMessage errors={errors} />
-        <Button type="submit">Create Account</Button>
-      </Form>
+          if (response && response.data && response.data.register.errors) {
+            setSubmitting(false);
+            return setErrors(normalizeErrors(response.data.register.errors));
+          } else {
+          }
+        }}
+        validationSchema={registerSchema}
+        validateOnBlur={false}
+        validateOnChange={false}
+      >
+        {({ errors, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <Field
+              name="username"
+              label="Username"
+              placeholder="Username"
+              component={InputField}
+            />
+            <Field
+              name="firstName"
+              label="First Name"
+              placeholder="First Name"
+              component={InputField}
+            />
+            <Field
+              name="lastName"
+              label="Last Name"
+              placeholder="Last Name"
+              component={InputField}
+            />
+            <Field
+              name="email"
+              label="Email"
+              placeholder="Email"
+              component={InputField}
+            />
+            <Field
+              name="password"
+              label="Password"
+              placeholder="Password"
+              component={InputField}
+              type="password"
+            />
+            <ErrorMessage errors={errors} />
+            <Button type="submit">Create Account</Button>
+          </Form>
+        )}
+      </Formik>
     )}
-  </Formik>
+  </Mutation>
 );
